c23e7a0f8cb34a192b0c9e7514fba1cdd13393b3
rolcho <roland@cimem.hu>
2024-10-03 17:00:42 +0200

    Longest Consecutives

---
+++ CSharp/ConsecutiveString/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsecutiveString;
+
+public class LongestConsecutives
+{
+    public static string LongestConsec(string[] strarr, int k)
+    {
+        if (strarr.Length == 0 || k > strarr.Length || k <= 0)
+        {
+            return "";
+        }
+
+        if (strarr.Length <= k)
+        {
+            return string.Join("", strarr);
+        }
+
+        List<string> longestStrings = new List<string>();
+        int maxLength = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            longestStrings.Add(strarr[i]);
+            maxLength += strarr[i].Length;
+        }
+
+        List<string> stringWindow = new List<string>(longestStrings);
+        int currentLength = maxLength;
+
+        for (int i = k; i < strarr.Length; i++)
+        {
+            currentLength = currentLength - stringWindow[0].Length + strarr[i].Length;
+            stringWindow.RemoveAt(0);
+            stringWindow.Add(strarr[i]);
+            if (currentLength > maxLength)
+            {
+                longestStrings = new List<string>(stringWindow);
+                maxLength = currentLength;
+            }
+        }
+        return string.Join("", longestStrings);
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(
+            LongestConsecutives.LongestConsec(
+                ["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"],
+                2
+            )
+        );
+    }
+}
