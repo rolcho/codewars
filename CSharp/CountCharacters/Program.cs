1614016da211e6a015feaaaabca707905dd2b224
rolcho <roland@cimem.hu>
2024-10-03 10:31:33 +0200

    count characters

---
+++ CSharp/CountCharacters/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CountCharacters;
+
+public class Kata
+{
+    public static Dictionary<char, int> Count(string str)
+    {
+        if (str.Length is 0)
+            return [];
+        var characters = new Dictionary<char, int>();
+
+        foreach (var character in str)
+        {
+            if (characters.ContainsKey(character))
+            {
+                characters[character]++;
+            }
+            else
+            {
+                characters.Add(character, 1);
+            }
+        }
+        return characters;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Kata.Count("Hello");
+    }
+}
---
+++ csharp/CountCharacters/CountCharacters.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+
+public class Kata
+{
+  public static Dictionary<char, int> Count(string str)
+  {
+         return new Dictionary<char, int>();
+  }
+}
+
+public void Main()
+{
+}
