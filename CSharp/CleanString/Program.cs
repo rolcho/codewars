71da69ffd403715a334652cd63fc14b55c04aac5
rolcho <roland@cimem.hu>
2024-10-07 11:44:57 +0200

    Clean String

---
+++ CSharp/CleanString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+static string CleanString(string s)
+{
+    var sb = new StringBuilder();
+
+    foreach (char c in s)
+    {
+        if (c == '#')
+        {
+            if (sb.Length > 0)
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+        }
+        else
+        {
+            sb.Append(c);
+        }
+    }
+
+    return sb.ToString();
+}
+
+Console.WriteLine(CleanString("#ab#c"));
