7e5743a1f4b79db52622bc3cec848e42db97cce0
rolcho <roland@cimem.hu>
2024-10-07 19:26:38 +0200

    Evaporator

---
+++ CSharp/Evaporator/Program.cs
@@ -0,0 +1,14 @@
+﻿static int Evaporator(double content, double evap_per_day, double threshold)
+{
+    int days = 0;
+    double thresholdAmount = threshold / 100 * content;
+    while (content > thresholdAmount)
+    {
+        content -= content * evap_per_day / 100;
+        days++;
+    }
+    return days;
+}
+
+Console.WriteLine(Evaporator(10, 10, 5));
+Console.WriteLine(Evaporator(10, 10, 10));
