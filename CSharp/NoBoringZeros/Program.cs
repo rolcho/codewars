276212ba59cd44e684d42fb918bf6ccb7c3b47e0
rolcho <roland@cimem.hu>
2024-10-07 11:28:55 +0200

    No Boring Zeros

---
+++ CSharp/NoBoringZeros/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NoBoringZeros;
+
+class Program
+{
+    public static int NoBoringZeros(int n)
+    {
+        if (n == 0)
+            return 0;
+        while (n % 10 == 0)
+            n /= 10;
+        return n;
+    }
+
+    static void Main(string[] args) { }
+}
