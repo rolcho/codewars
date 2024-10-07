d3807547ab960c4f2be468a02fd60db333149362
rolcho <roland@cimem.hu>
2024-10-07 19:15:47 +0200

    String Sum

---
+++ CSharp/SumTheStrings/Program.cs
@@ -0,0 +1,9 @@
+﻿static string StringsSum(string s1, string s2)
+{
+    // Write your solution here.
+    return (
+        (string.IsNullOrEmpty(s1) ? 0 : int.Parse(s1))
+        + (string.IsNullOrEmpty(s2) ? 0 : int.Parse(s2))
+    ).ToString();
+}
+Console.WriteLine(StringsSum("1", ""));
