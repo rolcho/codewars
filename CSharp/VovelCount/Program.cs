28bd7428b35af82da40eb45d93bd299047fc3079
rolcho <roland@cimem.hu>
2024-10-07 15:13:55 +0200

    Vovel Count

---
+++ CSharp/VovelCount/Program.cs
@@ -0,0 +1,15 @@
+﻿static int GetVowelCount(string str)
+{
+    int vowelCount = 0;
+    var vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+    foreach (char c in str)
+    {
+        if (vowels.Contains(c))
+        {
+            vowelCount++;
+        }
+    }
+    return vowelCount;
+}
+
+Console.WriteLine(GetVowelCount("hello"));
