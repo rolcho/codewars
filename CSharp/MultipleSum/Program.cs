c1644010202a85c3f7f7195085b1d6aac2fb8955
rolcho <roland@cimem.hu>
2024-10-07 20:12:16 +0200

    Multiple SUm

---
+++ CSharp/MultipleSum/Program.cs
@@ -0,0 +1,16 @@
+﻿static int MultipleSum(int value)
+{
+    int sum = 0;
+    for (int i = 3; i < value; i += 3)
+    {
+        sum += i;
+    }
+    for (int i = 5; i < value; i += 5)
+    {
+        if (i % 3 != 0)
+            sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine(MultipleSum(10));
