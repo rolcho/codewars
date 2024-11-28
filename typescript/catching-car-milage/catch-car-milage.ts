319ad719e4e5c4c7818cbca5995795541b685e79
rolcho <roland@cimem.hu>
2024-11-28 17:54:06 +0100

    basic rules

--- typescript/catching-car-milage/catch-car-milage.ts
+++ typescript/catching-car-milage/catch-car-milage.ts
@@ -7,11 +7,47 @@
 // The digits are a palindrome: 1221 or 73837
 // The digits match one of the values in the awesomePhrases array
 
-const convertToArray = (n: number) => n.toString().split("").map((d) => parseInt(d));
+const convertToArray = (n: number) =>
+  n
+    .toString()
+    .split("")
+    .map((d) => Number.parseInt(d));
 
+const rules = (nums: number[]): boolean => {
+  let isFollowedByZeros = true;
+  let isSameDigits = true;
+  let isIncrementDigits = true;
+  let isDecrementDigits = true;
+  let isPalindrome = true;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0) {
+      isFollowedByZeros = nums[i] === 0 && isFollowedByZeros;
+      isSameDigits = nums[i] === nums[i - 1] && isSameDigits;
+      isDecrementDigits = nums[i] === nums[i - 1] - 1 && isDecrementDigits;
+      isIncrementDigits = nums[i] === nums[i - 1] + 1 && isIncrementDigits;
+    }
+    isPalindrome = nums[i] === nums[nums.length - i - 1] && isPalindrome;
+  }
+
+  return (
+    isFollowedByZeros ||
+    isSameDigits ||
+    isIncrementDigits ||
+    isDecrementDigits ||
+    isPalindrome
+  );
+};
 export function isInteresting(n: number, awesomePhrases: number[]): number {
   const digits = convertToArray(n);
+  const result = rules(digits);
   return 0;
 }
 
-isInteresting(123, []);
+isInteresting(100, []);
+isInteresting(90000, []);
+isInteresting(1111, []);
+isInteresting(1234, []);
+isInteresting(4321, []);
+isInteresting(1221, []);
+isInteresting(73837, []);
