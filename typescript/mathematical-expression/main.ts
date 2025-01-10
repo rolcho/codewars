3e46ab477eedcfb20a7cdcecd07147fc0142b55d
rolcho <roland@cimem.hu>
2025-01-10 20:51:58 +0100

    ts: changes

--- typescript/mathematical-expression/main.ts
+++ typescript/mathematical-expression/main.ts
@@ -1,23 +1,23 @@
 type Operand = "*" | "/" | "+" | "-";
 
 type MathNode = {
-  value: Operand | number | undefined;
-  leftNode: MathNode | undefined;
-  rightNode: MathNode | undefined;
+  value: Operand | number;
+  leftNode?: MathNode;
+  rightNode?: MathNode;
 };
 
-const input = "2+3-4*5/6";
+const input = "2+3-1-4";
 
-function parseTree(input: string): MathNode | undefined {
+function parseTree(input: string, prevNode?: MathNode): MathNode | undefined {
   if (input.length === 0) return undefined;
   const hasOperand =
     input.includes("*") ||
     input.includes("/") ||
     input.includes("+") ||
     input.includes("-");
-  if (!isNaN(Number.parseInt(input)) && !hasOperand)
+  if (!isNaN(Number.parseFloat(input)) && !hasOperand)
     return {
-      value: Number.parseInt(input),
+      value: Number.parseFloat(input),
       leftNode: undefined,
       rightNode: undefined,
     };
@@ -34,12 +34,42 @@ function parseTree(input: string): MathNode | undefined {
   const operand = input[firstOperand] as Operand;
   const leftString = input.slice(0, firstOperand);
   const rightString = input.slice(firstOperand + 1);
-  return {
+  const leftNode = prevNode ?? { value: Number(leftString) };
+  const rightNode = parseTree(rightString, prevNode);
+  const newNode: MathNode = {
     value: operand,
-    leftNode: parseTree(leftString),
-    rightNode: parseTree(rightString),
+    leftNode,
+    rightNode,
   };
+  return newNode;
 }
 
-const tree = parseTree(input);
-console.log({ tree });
+function calculate(startNode: MathNode | undefined): number {
+  if (startNode === undefined) return 0;
+  switch (startNode.value) {
+    case "+":
+      const resultAdd =
+        calculate(startNode.leftNode) + calculate(startNode.rightNode);
+      return resultAdd;
+    case "-":
+      const resultMinus =
+        calculate(startNode.leftNode) - calculate(startNode.rightNode);
+      return resultMinus;
+    case "*":
+      const resultMultiply =
+        calculate(startNode.leftNode) * calculate(startNode.rightNode);
+      return resultMultiply;
+    case "/":
+      const divisor = calculate(startNode.rightNode);
+      if (divisor === 0) throw new Error("Division by zero");
+      const resultDivision = calculate(startNode.leftNode) / divisor;
+      return resultDivision;
+    default:
+      return startNode.value ?? 0;
+  }
+}
+
+const root = parseTree(input);
+
+const value = calculate(root);
+console.log(value);
