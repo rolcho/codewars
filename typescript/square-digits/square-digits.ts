a5f7cd58ed5f0904c01d54c87025161ad38b05c0
rolcho <roland@cimem.hu>
2025-01-19 22:14:20 +0100

    ts: square digits

---
+++ typescript/square-digits/square-digits.ts
@@ -0,0 +1,10 @@
+export class Kata {
+  static squareDigits(num: number): number {
+    return Number.parseInt(
+      String(num)
+        .split("")
+        .map((s: string) => String(Math.pow(Number.parseInt(s), 2)))
+        .join("")
+    );
+  }
+}
