d6a0351cd9674ec90554a15dbe76ae45b1db0466
rolcho <roland@cimem.hu>
2025-01-20 12:00:02 +0100

    refactor: scramblies

--- typescript/scramblies/scramblies.ts
+++ typescript/scramblies/scramblies.ts
@@ -1,17 +1,13 @@
 export function scramble(str1: string, str2: string): boolean {
   const letters: Record<string, number> = {};
-  const wordLetters: Record<string, number> = {};
 
   for (const letter of str1) {
     letters[letter] = (letters[letter] ?? 0) + 1;
   }
 
   for (const letter of str2) {
-    wordLetters[letter] = (wordLetters[letter] ?? 0) + 1;
-  }
-
-  for (const [letter, count] of Object.entries(wordLetters)) {
-    if (letters[letter] === undefined || letters[letter] < count) return false;
+    if (!letters[letter]) return false;
+    letters[letter]--;
   }
 
   return true;
