1c0620c0ef9e55be5c1abab2ae3e2c0f9337eae6
rolcho <roland@cimem.hu>
2025-03-15 09:37:20 +0100

    ts: Add createPhoneNumber function with tests

---
+++ typescript/create-phone-number/create.ts
@@ -0,0 +1,27 @@
+/**
+ * Creates a formatted phone number string from an array of numbers.
+ *
+ * @param numbers - An array of 10 numbers representing the digits of a phone number
+ * @returns A promise that resolves to a formatted phone number string in the format "(XXX) XXX-XXXX"
+ * @throws Error if the input array doesn't contain exactly 10 digits
+ * @throws Error if any element in the array is not a valid non-negative integer
+ *
+ * @example
+ * // Returns "(123) 456-7890"
+ * createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+ */
+export function createPhoneNumber(numbers: number[]): string {
+  if (numbers.length !== 10) throw new Error("Invalid number of digits");
+  numbers.forEach((num) => {
+    if (Number.isNaN(num) || Math.floor(num) !== num || num < 0)
+      throw new Error("Invalid digit");
+  });
+  const phoneNumber =
+    "(" +
+    numbers.splice(0, 3).join("") +
+    ") " +
+    numbers.splice(0, 3).join("") +
+    "-" +
+    numbers.join("");
+  return phoneNumber;
+}
