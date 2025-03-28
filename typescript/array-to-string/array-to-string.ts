da4f11f4a7a516731a749dc38854211540843585
rolcho <roland@cimem.hu>
2025-03-28 16:09:51 +0100

    ts: array to string started

--- typescript/array-to-string/array-to-string.ts
+++ typescript/array-to-string/array-to-string.ts
@@ -3,6 +3,22 @@ export function arrayToString(arr: number[]): string {
 }
 
 export function stringToArray(str: string): number[] {
+  const getElementsFromString = (s: string): string[] => {
+    return s.split(',');
+  }
+  const getArrayFromSlice = (s: string): number[] => {
+    const increment = s.split('+')[1];
+    const decrement = s.split('-')[1];
 
+    if (increment === decrement) return [Number.parseInt(s))];
+
+    const [startNumber, 
+  }
+  const numbers: number[] = [];
+  const items = getElementsFromString(str);
+  for (const item of items) {
+    const numbersSlice = []
+    numbers.push(...numbersSlice);
+  }
   return [0];
 }
