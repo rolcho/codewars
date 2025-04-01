31359d09a45034cd6dc3a5c9ce901fec1d43f58c
Roland Nagy <roland@cimem.hu>
2025-04-01 23:06:19 +0200

    ts: biome config update

--- typescript/array-to-string/array-to-string.ts
+++ typescript/array-to-string/array-to-string.ts
@@ -1,13 +1,14 @@
 export function arrayToString(arr: number[]): string {
+  console.log(arr.flat());
   return '';
 }
 
 export function stringToArray(str: string): number[] {
   const getElementsFromString = (s: string): string[] => {
     return s.split(',');
-  }
+  };
 
-  type Iteration = { start: number, iteration: number, increment: number };
+  type Iteration = { start: number; iteration: number; increment: number };
 
   const splitSlice = (startAndIteration: string, inc: string): Iteration => {
     const [startStr, iterationStr] = startAndIteration.split(':');
@@ -15,32 +16,38 @@ export function stringToArray(str: string): number[] {
     const iteration = Number.parseInt(iterationStr);
     const increment = Number.parseInt(inc);
     return { start, iteration, increment };
-  }
+  };
 
   const createArray = (i: Iteration): number[] => {
     const { start, iteration, increment } = i;
     const filledArray: number[] = [];
     for (let i = 0; i < iteration; i++) {
-      filledArray.push(start + (increment * i));
+      filledArray.push(start + increment * i);
     }
-    return (filledArray);
-  }
+    return filledArray;
+  };
 
   const getArrayFromSlice = (s: string): number[] => {
     const [startAndIterationPlus, incrementNumber] = s.split('+');
     if (startAndIterationPlus !== s) {
-      const { start, iteration, increment } = splitSlice(startAndIterationPlus, incrementNumber);
+      const { start, iteration, increment } = splitSlice(
+        startAndIterationPlus,
+        incrementNumber,
+      );
       return createArray({ start, iteration, increment });
     }
     const [startAndIterationMinus, decrementNumber] = s.split('-');
-    if (startAndIterationMinus != s) {
-      const { start, iteration, increment } = splitSlice(startAndIterationMinus, decrementNumber);
+    if (startAndIterationMinus !== s) {
+      const { start, iteration, increment } = splitSlice(
+        startAndIterationMinus,
+        decrementNumber,
+      );
       return createArray({ start, iteration, increment: -increment });
     }
-    const { start, iteration, increment } = splitSlice(s, "0");
+    const { start, iteration, increment } = splitSlice(s, '0');
     if (Number.isNaN(iteration)) return [Number.parseInt(s)];
     return createArray({ start, iteration, increment });
-  }
+  };
 
   const numbers: number[] = [];
   const items = getElementsFromString(str);
@@ -51,4 +58,4 @@ export function stringToArray(str: string): number[] {
   return numbers;
 }
 
-console.log(stringToArray("1:3+1,6:6-2,9,5:3,8"));
+console.log(stringToArray('1:3+1,6:6-2,9,5:3,8'));
